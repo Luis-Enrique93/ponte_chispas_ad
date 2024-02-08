bb3dbb9133016fc31be96cd8fb491eb9fb120ab6
Luis-Enrique93 <l.enrique.m93@gmail.com>
2024-02-08 14:58:09 -0300

    'produccion'

--- auto.py
+++ auto.py
@@ -6,7 +6,7 @@ class Auto:
     def start():
         print('--- start ---')
         subprocess.call('git pull origin master', shell=True)
-        subprocess.call('sudo docker-compose -f docker-compose.prod.yaml up --build -d', shell=True)
+        subprocess.call('sudo docker-compose -f docker-compose.yaml up --build -d', shell=True)
         subprocess.call('docker system prune -a', shell=True)
         print('--- end ---')
 
