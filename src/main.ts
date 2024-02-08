42b74868a19c9c26ace2d3eeea1625ac8564961d
Luis-Enrique93 <l.enrique.m93@gmail.com>
2024-02-08 18:33:41 -0300

    'produccion'

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() 
 {
-    ActiveDirectory.Start(
-        process.env.ACTIVE_DIRECTORY_IP2,
-        process.env.ACTIVE_DIRECTORY_PORT,
-        process.env.ACTIVE_DIRECTORY_USERNAME,
-        process.env.ACTIVE_DIRECTORY_PASSWORD
-    )
+    // ActiveDirectory.Start(
+    //     process.env.ACTIVE_DIRECTORY_IP2,
+    //     process.env.ACTIVE_DIRECTORY_PORT,
+    //     process.env.ACTIVE_DIRECTORY_USERNAME,
+    //     process.env.ACTIVE_DIRECTORY_PASSWORD
+    // )
 
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
