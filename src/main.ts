2bf455f1606801cbc43c969ef4996a3eb6306b40
Luis-Enrique93 <l.enrique.m93@gmail.com>
2024-02-09 15:14:20 -0300

    'produccion'

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap()
 
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-    await app.listen(3000)
+    await app.listen(process.env.PORT || 4000)
 }
 
 
