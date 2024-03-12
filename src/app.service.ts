b4165af1ee1568e050195402331643b744c57562
Luis-Enrique93 <l.enrique.m93@gmail.com>
2024-03-12 19:57:21 -0300

    'produccion'

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { ActiveDirectory } from './lib/active.directory'
 import { SearchOptions } from 'ldapjs'
+import { Cron } from '@nestjs/schedule'
 
 
 @Injectable()
@@ -8,9 +9,48 @@ export class AppService
 {
     public opts: SearchOptions = {
         filter: '(objectClass=*)',
-        // scope: 'base', // no ver sub carpetas
         scope: 'sub', // para ver sub carpetas
-        // attributes: ['sn', 'cn']
+    }
+
+
+    public async searchUsers(search: string = 'OU=Agents,OU=Banrural I GT,OU=Users Production W10,DC=BOT,DC=corp'): Promise<any>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const data = []
+
+            ActiveDirectory.client.search(search, this.opts, (err, res) =>
+            {
+                res.on('searchEntry', (entry) =>
+                {
+                    const obj = {}
+
+                    obj['objectName'] = entry?.pojo?.objectName
+
+                    for (const value of entry?.pojo?.attributes)
+                    {
+                        obj[value?.type] = value?.values.length > 1 ? value.values : value?.values[0]
+                    }
+
+                    console.log(obj)
+
+                    data.push(obj)
+                })
+
+                res.on('error', (err) =>
+                {
+                    console.error('error: ' + err.message)
+                    reject(err)
+                })
+
+                res.on('end', (result) =>
+                {
+                    console.log('ejecutando end')
+                    console.log('status: ' + result.status)
+                    resolve(data)
+                })
+            })
+        })
     }
 
 
@@ -78,4 +118,28 @@ export class AppService
             })
         })
     }
+
+
+    // @Cron('*/3 * * * *')
+    // public handleCron(): void
+    // {
+    //     ActiveDirectory.client.search("OU=Agents,OU=Banrural I GT,OU=Users Production W10,DC=BOT,DC=corp", this.opts, (err, res) =>
+    //     {
+    //         res.on('searchEntry', (entry) =>
+    //         {
+    //             console.log(`cron: ${entry.pojo.objectName}`)
+    //         })
+
+    //         res.on('error', (err) =>
+    //         {
+    //             console.error('error: ' + err.message)
+    //         })
+
+    //         res.on('end', (result) =>
+    //         {
+    //             console.log('ejecutando end')
+    //             console.log('status: ' + result.status)
+    //         })
+    //     })
+    // }
 }
